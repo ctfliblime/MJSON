c80a97361164828474ce5ce9debe50da9f1a69bb
Clay Fouts <cfouts@liblime.com>
2012-03-02 19:23:44 -0800

    Move lexer/parser into packages, break out test data

--- jparse.pl
+++ jparse.pl
@@ -1,10 +1,8 @@
 #!/usr/bin/env perl
 
 use Modern::Perl;
-
-use Marpa::XS;
-use Log::Dispatch;
 use Data::Dumper;
+use Log::Dispatch;
 
 our $log = Log::Dispatch->new(
     outputs => [
@@ -12,110 +10,119 @@ our $log = Log::Dispatch->new(
     ],
 );
 
-my $json1 = <<EOF;
-[
-"q",
-"w",
-{"A" : "B", "C" : "D"},
-"e
-",
-"r"
-]
-EOF
-
-my $json2 = q{"A\"B"};
-
-my $json3 = <<EOF;
 {
-    "glossary": {
-        "title": "example glossary",
-        "GlossDiv": {
-            "title": "S",
-            "GlossList": {
-                "GlossEntry": {
-                    "ID": "SGML",
-                    "SortAs": "SGML",
-                    "GlossTerm": "Standard Generalized Markup Language",
-                    "Acronym": "SGML",
-                    "Abbrev": "ISO 8879:1986",
-                    "GlossDef": {
-                        "para": "A meta-markup language, used to create markup languages such as DocBook.",
-                        "GlossSeeAlso": ["GML", "XML"]
-                    },
-                    "GlossSee": "markup"
-                }
+    package MJSON::Tokenize;
+
+    sub token_itr {
+        my $json = shift;
+
+        return sub {
+          TOKEN: {
+                return ['COMMA']           if $json =~ /\G ,        /gcx;
+                return ['COLON']           if $json =~ /\G :        /gcx;
+                return ['OPEN_BRACKET']    if $json =~ /\G \[       /gcx;
+                return ['CLOSE_BRACKET']   if $json =~ /\G \]       /gcx;
+                return ['OPEN_CURLY']      if $json =~ /\G {        /gcx;
+                return ['CLOSE_CURLY']     if $json =~ /\G }        /gcx;
+                return ['STRING' => $1]    if $json =~ /\G "([^"]*)"/gcx;
+                return ['NULL']            if $json =~ /\G null     /gcx;
+                redo TOKEN                 if $json =~ /\G \s+      /gcx;
+                return undef;
             }
-        }
+        };
     }
+
+    sub tokenize {
+        my $json = shift;
+        my $itr = token_itr($json);
+        my @tokens;
+
+        my $t;
+        push @tokens, $t while ($t = $itr->());
+
+        $log->info(Dumper \@tokens);
+
+        return @tokens;
+    }
+
+    1;
 }
-EOF
 
-my $json4 = <<EOF;
-{"menu": {
-    "header": "SVG Viewer",
-    "items": [
-        {"id": "Open"},
-        {"id": "OpenNew", "label": "Open New"},
-        null,
-        {"id": "ZoomIn", "label": "Zoom In"},
-        {"id": "ZoomOut", "label": "Zoom Out"},
-        {"id": "OriginalView", "label": "Original View"},
-        null,
-        {"id": "Quality"},
-        {"id": "Pause"},
-        {"id": "Mute"},
-        null,
-        {"id": "Find", "label": "Find..."},
-        {"id": "FindAgain", "label": "Find Again"},
-        {"id": "Copy"},
-        {"id": "CopyAgain", "label": "Copy Again"},
-        {"id": "CopySVG", "label": "Copy SVG"},
-        {"id": "ViewSVG", "label": "View SVG"},
-        {"id": "ViewSource", "label": "View Source"},
-        {"id": "SaveAs", "label": "Save As"},
-        null,
-        {"id": "Help"},
-        {"id": "About", "label": "About Adobe CVG Viewer..."}
-    ]
-}}
-EOF
+{
+    package MJSON;
 
-sub token_itr {
-    my $json = shift;
+    sub do_default {
+        $log->debug(Data::Dumper->Dump([ [@_] ],[ 'default' ]));
+        return $_[1];
+    }
 
-    return sub {
-        TOKEN: {
-            return ['COMMA']           if $json =~ /\G ,        /gcx;
-            return ['COLON']           if $json =~ /\G :        /gcx;
-            return ['OPEN_BRACKET']    if $json =~ /\G \[       /gcx;
-            return ['CLOSE_BRACKET']   if $json =~ /\G \]       /gcx;
-            return ['OPEN_CURLY']      if $json =~ /\G {        /gcx;
-            return ['CLOSE_CURLY']     if $json =~ /\G }        /gcx;
-            return ['STRING' => $1]    if $json =~ /\G "([^"]*)"/gcx;
-            return ['NULL']            if $json =~ /\G null     /gcx;
-            redo TOKEN                 if $json =~ /\G \s+      /gcx;
-            return undef;
+    sub null {
+        $log->debug(Data::Dumper->Dump([ [@_] ],[ 'null' ]));
+        return undef;
+    }
+
+    sub hash {
+        $log->debug(Data::Dumper->Dump([ [@_] ],[ 'hash' ]));
+        return $_[2];
+    }
+
+    sub key_value_pairs {
+        $log->debug(Data::Dumper->Dump([ [@_] ],[ 'key_value_pairs' ]));
+        my %h;
+        for (keys %{$_[1]}) {
+            $h{$_} = $_[1]->{$_};
         }
-    };
+        for (keys %{$_[3]}) {
+            $h{$_} = $_[3]->{$_};
+        }
+        return \%h;
+    }
+
+    sub key_value_pair {
+        $log->debug(Data::Dumper->Dump([ [@_] ],[ 'key_value_pair' ]));
+        return {$_[1] => $_[3]};
+    }
+
+    sub array {
+        $log->debug(Data::Dumper->Dump([ [@_] ],[ 'array' ]));
+        return $_[2];
+    }
+
+    sub array_elements {
+        $log->debug(Data::Dumper->Dump([ [@_] ],[ 'array_elements' ]));
+        my @a = map {$_} $_[1], @{$_[3]};
+        return \@a;
+    }
+
+    sub array_element {
+        $log->debug(Data::Dumper->Dump([ [@_] ],[ 'array_element' ]));
+        return $_[1];
+    }
+
+    sub some_data {
+        $log->debug(Data::Dumper->Dump([ [@_] ],[ 'some_data' ]));
+        return $_[1];
+    }
+
+    sub string {
+        $log->debug("string:$_[1]");
+        return $_[1];
+    }
+
+    sub json {
+        $log->debug(Data::Dumper->Dump([ [@_] ],[ 'json' ]));
+        return $_[1];
+    }
+
+    1;
 }
 
-sub tokenize {
-    my $json = shift;
-    my $itr = token_itr($json);
-    my @tokens;
-
-    my $t;
-    push @tokens, $t while ($t = $itr->());
-    return @tokens;
-}
-
-my @tokens = tokenize($json4);
-
-$log->info(Dumper \@tokens);
+use Marpa::XS;
+use IO::All;
 
 my $grammar = Marpa::XS::Grammar->new({
     start => 'json',
-    actions => 'main',
+    actions => 'MJSON',
     default_action => 'do_default',
     terminals => [qw(
         OPEN_CURLY CLOSE_CURLY
@@ -154,9 +161,13 @@ my $grammar = Marpa::XS::Grammar->new({
 });
 
 $grammar->precompute;
-
 my $rec = Marpa::XS::Recognizer->new( { grammar => $grammar } );
-foreach my $token (@tokens) {
+
+my $json < io '-';
+
+my @tokens = MJSON::Tokenize::tokenize($json);
+
+for my $token (@tokens) {
     if (defined $rec->read( @$token )) {
         $log->debug("reading Token: @$token");
     } else {
@@ -166,66 +177,3 @@ foreach my $token (@tokens) {
 
 my $output = ${$rec->value};
 $log->notice(Data::Dumper->Dump([$output], ['final']));
-
-sub do_default {
-    $log->debug(Data::Dumper->Dump([ [@_] ],[ 'default' ]));
-    return $_[1];
-}
-
-sub null {
-    $log->debug(Data::Dumper->Dump([ [@_] ],[ 'null' ]));
-    return undef;
-}
-
-sub hash {
-    $log->debug(Data::Dumper->Dump([ [@_] ],[ 'hash' ]));
-    return $_[2];
-}
-
-sub key_value_pairs {
-    $log->debug(Data::Dumper->Dump([ [@_] ],[ 'key_value_pairs' ]));
-    my %h;
-    for (keys %{$_[1]}) {
-        $h{$_} = $_[1]->{$_};
-    }
-    for (keys %{$_[3]}) {
-        $h{$_} = $_[3]->{$_};
-    }
-    return \%h;
-}
-
-sub key_value_pair {
-    $log->debug(Data::Dumper->Dump([ [@_] ],[ 'key_value_pair' ]));
-    return {$_[1] => $_[3]};
-}
-
-sub array {
-    $log->debug(Data::Dumper->Dump([ [@_] ],[ 'array' ]));
-    return $_[2];
-}
-
-sub array_elements {
-    $log->debug(Data::Dumper->Dump([ [@_] ],[ 'array_elements' ]));
-    my @a = map {$_} $_[1], @{$_[3]};
-    return \@a;
-}
-
-sub array_element {
-    $log->debug(Data::Dumper->Dump([ [@_] ],[ 'array_element' ]));
-    return $_[1];
-}
-
-sub some_data {
-    $log->debug(Data::Dumper->Dump([ [@_] ],[ 'some_data' ]));
-    return $_[1];
-}
-
-sub string {
-    $log->debug("string:$_[1]");
-    return $_[1];
-}
-
-sub json {
-    $log->debug(Data::Dumper->Dump([ [@_] ],[ 'json' ]));
-    return $_[1];
-}
